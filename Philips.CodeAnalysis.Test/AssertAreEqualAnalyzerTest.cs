7d2c19c49ec1c52e3f9d4e5f938ce995ba4d3d10
Mayer, Jean-Paul <4032461+mayerj@users.noreply.github.com>
2020-08-24 09:21:45 -0400

    Handle more cases in the <actual> -> <expected> Assert.AreEqual analyzer.  Handle -1, as a constant.

--- Philips.CodeAnalysis.Test/AssertAreEqualAnalyzerTest.cs
+++ Philips.CodeAnalysis.Test/AssertAreEqualAnalyzerTest.cs
@@ -54,6 +54,68 @@ Assert.AreEqual(default, GetValue());
 ");
 		}
 
+		[DataRow(true, null, "-1", true)]
+		[DataRow(true, null, "1", true)]
+		[DataRow(true, null, "0", true)]
+		[DataRow(true, null, "-1u", true)]
+		[DataRow(true, null, "1u", true)]
+		[DataRow(true, null, "0u", true)]
+		[DataRow(true, "-1", null, false)]
+		[DataRow(true, "1", null, false)]
+		[DataRow(true, "0", null, false)]
+		[DataRow(true, "-1u", null, false)]
+		[DataRow(true, "1u", null, false)]
+		[DataRow(true, "0u", null, false)]
+		[DataRow(false, null, "-1", true)]
+		[DataRow(false, null, "1", true)]
+		[DataRow(false, null, "0", true)]
+		[DataRow(false, null, "-1u", true)]
+		[DataRow(false, null, "1u", true)]
+		[DataRow(false, null, "0u", true)]
+		[DataRow(false, "-1", null, false)]
+		[DataRow(false, "1", null, false)]
+		[DataRow(false, "0", null, false)]
+		[DataRow(false, "-1u", null, false)]
+		[DataRow(false, "1u", null, false)]
+		[DataRow(false, "0u", null, false)]
+		[DataTestMethod]
+		public void CheckNegativeInteger(bool shouldWrapArgument, string arg0, string arg1, bool isError)
+		{
+			string expectedParameter = arg0 ?? "GetValue()";
+			string actualParameter = arg1 ?? "GetValue()";
+
+			if (shouldWrapArgument)
+			{
+				if (arg0 is null)
+				{
+					expectedParameter = $"({expectedParameter})";
+				}
+
+				if (arg1 is null)
+				{
+					actualParameter = $"({actualParameter})";
+				}
+			}
+
+			string template = @$"
+int GetValue()
+{{
+	return 0;
+}}
+
+Assert.AreEqual({expectedParameter}, {actualParameter});
+";
+
+			if (isError)
+			{
+				VerifyError(template);
+			}
+			else
+			{
+				VerifyNoError(template);
+			}
+		}
+
 		[TestMethod]
 		public void CheckWillIgnoreTypeArgument()
 		{
