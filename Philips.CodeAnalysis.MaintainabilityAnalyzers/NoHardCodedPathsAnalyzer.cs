3bb9bc66840c656c9f0eef3dba25da9ea2369a0e
deepak kumar <deepak94.mce@gmail.com>
2021-01-21 09:11:38 -0500

    Fixed comments and message to reflect absolute path

--- Philips.CodeAnalysis.MaintainabilityAnalyzers/NoHardCodedPathsAnalyzer.cs
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/NoHardCodedPathsAnalyzer.cs
@@ -1,10 +1,4 @@
-﻿using System.Collections.Immutable;
-using System.Text.RegularExpressions;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Diagnostics;
-using Philips.CodeAnalysis.Common;
+﻿using System.Text.RegularExpressions;
 
 namespace Philips.CodeAnalysis.MaintainabilityAnalyzers
 {
@@ -17,9 +11,9 @@ namespace Philips.CodeAnalysis.MaintainabilityAnalyzers
 	{
 
 		#region Non-Public Data Members
-		private const string Title = @"Paths must not be hardcoded";
-		private const string MessageFormat = @"Paths must not be hardcoded";
-		private const string Description = @"Paths must not be hardcoded";
+		private const string Title = @"Absolute paths must not be hardcoded";
+		private const string MessageFormat = @"Absolute paths must not be hardcoded";
+		private const string Description = @"Absolute paths must not be hardcoded";
 		private const string Category = Categories.Maintainability;
 		private Regex Pattern = new Regex(@"^[a-zA-Z]:\\(((?![<>:/\\|?*]).)+((?<![ .])\\)?)*$");
 		#endregion
@@ -28,7 +22,7 @@ namespace Philips.CodeAnalysis.MaintainabilityAnalyzers
 		private void Analyze(SyntaxNodeAnalysisContext context)
 		{
 			LiteralExpressionSyntax stringLiteralExpressionNode = (LiteralExpressionSyntax)context.Node;
-			// Get the text value of the literal expression.
+			// Get the text value of the string literal expression.
 			string pathValue = stringLiteralExpressionNode.Token.ValueText;
 			// If the pattern matches the text value, report the diagnostic.
 			if (Pattern.IsMatch(pathValue))
