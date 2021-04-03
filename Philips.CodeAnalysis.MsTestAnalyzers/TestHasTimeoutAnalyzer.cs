248bab4aca4b9b563b18ef7759946c21a93c445c
Mayer, Jean-Paul <4032461+mayerj@users.noreply.github.com>
2021-04-03 10:58:24 -0400

    initial implementation

---
+++ Philips.CodeAnalysis.MsTestAnalyzers/TestHasTimeoutAnalyzer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Philips.CodeAnalysis.Common;
+
+namespace Philips.CodeAnalysis.MsTestAnalyzers
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class TestHasTimeoutAttributeAnalyzer : TestMethodDiagnosticAnalyzer
+	{
+		private const string Title = @"Test must have an appropriate Timeout";
+		public const string MessageFormat = @"Test must have an appropriate Timeout attribute.";
+		private const string Description = @"Tests that lack a Timeout may indefinitely block.";
+		private const string Category = Categories.Maintainability;
+
+		private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(Helper.ToDiagnosticId(DiagnosticIds.TestHasTimeoutAttribute),
+												Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
+
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+		protected override void OnTestMethod(SyntaxNodeAnalysisContext context, MethodDeclarationSyntax methodDeclaration, bool isDataTestMethod)
+		{
+			SyntaxList<AttributeListSyntax> attributeLists = methodDeclaration.AttributeLists;
+
+			if (!Helper.HasAttribute(attributeLists, context, MsTestFrameworkDefinitions.TimeoutAttribute, out Location timeoutLocation, out string argument))
+			{
+				Diagnostic diagnostic = Diagnostic.Create(Rule, methodDeclaration.GetLocation());
+				context.ReportDiagnostic(diagnostic);
+				return;
+			}
+
+			if (Helper.HasAttribute(attributeLists, context, MsTestFrameworkDefinitions.TestCategoryAttribute, out Location categoryLocation, out string category)
+				&& IsIncorrectTimeout(argument, category, out string errorText))
+			{
+				DiagnosticDescriptor Rule = new DiagnosticDescriptor(Helper.ToDiagnosticId(DiagnosticIds.TestHasTimeoutAttribute),
+																   Title, errorText, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
+				Diagnostic diagnostic = Diagnostic.Create(Rule, timeoutLocation);
+				context.ReportDiagnostic(diagnostic);
+			}
+		}
+
+		public const string UnitTestTimeoutIncorrect = "TestTimeout for Category UnitTests should be either CiAppropriate or CiAcceptable";
+		public const string IntegrationTimeoutIncorrect = "TestTimeout for Category Integration should be Integration";
+		public const string SmokeTimeoutIncorrect = "TestTimeout for Category Smoke should be Smoke";
+
+		public static bool IsIncorrectTimeout(string argument, string category, out string messageFormat)
+		{
+			if (category.Equals("TestDefinitions.UnitTests") && (!argument.Equals("TestTimeouts.CiAppropriate") && !argument.Equals("TestTimeouts.CiAcceptable")))
+			{
+				messageFormat = UnitTestTimeoutIncorrect;
+				return true;
+			}
+			else if (category.Equals("TestDefinitions.IntegrationTests") && !argument.Equals("TestTimeouts.Integration"))
+			{
+				messageFormat = IntegrationTimeoutIncorrect;
+				return true;
+			}
+			else if (category.Equals("TestDefinitions.SmokeTests") && !argument.Equals("TestTimeouts.Smoke"))
+			{
+				messageFormat = SmokeTimeoutIncorrect;
+				return true;
+			}
+
+			messageFormat = string.Empty;
+			return false;
+		}
+	}
+}
+
---
+++ Philips.CodeAnalysis.MsTestAnalyzers/TestHasTimeoutCodeFixProvider.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using Philips.CodeAnalysis.Common;
+
+namespace Philips.CodeAnalysis.MsTestAnalyzers
+{
+	[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(TestHasTimeoutCodeFixProvider)), Shared]
+	public class TestHasTimeoutCodeFixProvider : CodeFixProvider
+	{
+		private const string Title = "Add Test Timeout";
+
+		public sealed override ImmutableArray<string> FixableDiagnosticIds
+		{
+			get { return ImmutableArray.Create(Helper.ToDiagnosticId(DiagnosticIds.TestHasTimeoutAttribute)); }
+		}
+
+		public sealed override FixAllProvider GetFixAllProvider()
+		{
+			// See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+			return WellKnownFixAllProviders.BatchFixer;
+		}
+
+		public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+		{
+			SyntaxNode root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+			Diagnostic diagnostic = context.Diagnostics.First();
+			TextSpan diagnosticSpan = diagnostic.Location.SourceSpan;
+
+			// Find the method declaration identified by the diagnostic.
+			MethodDeclarationSyntax attributeList = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<MethodDeclarationSyntax>().First();
+
+			// Register a code action that will invoke the fix.
+			context.RegisterCodeFix(
+				CodeAction.Create(
+					title: Title,
+					createChangedDocument: c => AddTestTimeout(context.Document, attributeList, c),
+					equivalenceKey: Title),
+				diagnostic);
+		}
+
+		private async Task<Document> AddTestTimeout(Document document, MethodDeclarationSyntax method, CancellationToken cancellationToken)
+		{
+			SyntaxList<AttributeListSyntax> attributeLists = method.AttributeLists;
+			string category = string.Empty;
+			string timeout = string.Empty;
+			foreach (AttributeListSyntax attributes in attributeLists)
+			{
+				foreach (AttributeSyntax attributesyntax in attributes.Attributes)
+				{
+					if (attributesyntax.Name.ToString().Contains(@"Timeout"))
+					{
+						return document;
+					}
+					else if (attributesyntax.Name.ToString().Contains(@"TestCategory"))
+					{
+						category = attributesyntax.ArgumentList.Arguments.First().ToString();
+					}
+				}
+			}
+
+			switch (category)
+			{
+				case "TestDefinitions.UnitTests":
+					timeout = "(TestTimeouts.CiAppropriate)";
+					break;
+				case "TestDefinitions.IntegrationTests":
+					timeout = "(TestTimeouts.Integration)";
+					break;
+				case "TestDefinitions.NightlyTests":
+					timeout = "(TestTimeouts.Nightly)";
+					break;
+				case "TestDefinitions.SmokeTests":
+					timeout = "(TestTimeouts.Smoke)";
+					break;
+				default:
+					timeout = "(TestTimeouts.CiAppropriate)";
+					break;
+			}
+
+			NameSyntax name = SyntaxFactory.ParseName("Timeout");
+			SyntaxNode rootNode = await document.GetSyntaxRootAsync(cancellationToken);
+			AttributeArgumentListSyntax arguments = SyntaxFactory.ParseAttributeArgumentList(timeout);
+			AttributeSyntax attribute = SyntaxFactory.Attribute(name, arguments);
+
+			AttributeListSyntax attributeList = SyntaxFactory.AttributeList(
+				SyntaxFactory.SingletonSeparatedList<AttributeSyntax>(attribute));
+
+			MethodDeclarationSyntax newMethod = method.WithAttributeLists(method.AttributeLists.Add(attributeList));
+
+			SyntaxNode newRoot = rootNode.ReplaceNode(method, newMethod);
+			Document newDocument = document.WithSyntaxRoot(newRoot);
+			return newDocument;
+		}
+	}
+}
