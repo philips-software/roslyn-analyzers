a42bbcbc1545d991986f2585d7fbe75c4abe1f1d
Robert Connelly <61849411+rdconnelly@users.noreply.github.com>
2022-03-22 08:39:11 -0400

    Update LocationRangeModel.cs

--- Philips.CodeAnalysis.MaintainabilityAnalyzers/LocationRangeModel.cs
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/LocationRangeModel.cs
@@ -1,5 +1,6 @@
 ﻿// © 2019 Koninklijke Philips N.V. See License.md in the project root for license information.
 
+
 namespace Philips.CodeAnalysis.MaintainabilityAnalyzers
 {
 	public class LocationRangeModel
