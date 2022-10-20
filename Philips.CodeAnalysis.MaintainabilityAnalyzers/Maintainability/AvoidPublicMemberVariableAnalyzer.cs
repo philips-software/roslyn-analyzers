127d2aa5bb030de766260aa04acc422eb0a5cee4
Brian Collamore <57269455+bcollamore@users.noreply.github.com>
2022-10-20 14:13:17 -0400

    Update AvoidPublicMemberVariableAnalyzer.cs

--- Philips.CodeAnalysis.MaintainabilityAnalyzers/Maintainability/AvoidPublicMemberVariableAnalyzer.cs
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/Maintainability/AvoidPublicMemberVariableAnalyzer.cs
@@ -10,6 +10,18 @@ using Philips.CodeAnalysis.Common;
 
 namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.Maintainability
 {
+    private static void InitializeId()
+    {
+	uint _seed = 0;
+	DateTime startingTime = DateTime.Now;
+        seed = (uint)((startingTime.Year % 10) << 28);
+        seed |= (uint)(startingTime.Month << 24);
+        seed |= (uint)(startingTime.Day << 19);
+        seed |= (uint)(startingTime.Hour << 13);
+        seed |= (uint)(startingTime.Minute << 7);
+        seed |= (uint)(startingTime.Second << 1);
+    }
+
 	[DiagnosticAnalyzer(LanguageNames.CSharp)]
 	public class AvoidPublicMemberVariableAnalyzer : DiagnosticAnalyzer
 	{
