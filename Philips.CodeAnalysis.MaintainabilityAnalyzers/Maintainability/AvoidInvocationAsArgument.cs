8922f6b26b82a91457b0b3ca4e160407ef429081
Collamore, Brian <brian.collamore@philips.com>
2022-12-22 11:09:10 -0500

    Extract Local Variable

---
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/Maintainability/AvoidInvocationAsArgument.cs
@@ -0,0 +1,92 @@
+﻿// © 2022 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+using Philips.CodeAnalysis.Common;
+
+namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.Maintainability
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class AvoidInvocationAsArgumentAnalyzer : DiagnosticAnalyzer
+	{
+		private const string Title = @"Avoid method calls as arguments";
+		public const string MessageFormat = @"Avoid '{0}' as an argument";
+		private const string Description = @"Avoid method calls as arguments to method calls";
+		private const string Category = Categories.Maintainability;
+
+		private static readonly DiagnosticDescriptor Rule = new(Helper.ToDiagnosticId(DiagnosticIds.AvoidInvocationAsArgument), Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+			context.EnableConcurrentExecution();
+			context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.Argument);
+		}
+
+		private void Analyze(SyntaxNodeAnalysisContext context)
+		{
+			ArgumentSyntax argumentSyntax = (ArgumentSyntax)context.Node;
+
+			// We are looking for method calls as arguments
+			if (argumentSyntax.Expression is not InvocationExpressionSyntax argumentExpressionSyntax)
+			{
+				return;
+			}
+
+			// If it's an embedded nameof() operation, let it go.
+			if ((argumentExpressionSyntax.Expression as IdentifierNameSyntax)?.Identifier.Text == "nameof")
+			{
+				return;
+			}
+
+			// If it's calling ToString(), let it go. (ToStrings() cognitive load isn't excessive, and lots of violations)
+			if ((argumentExpressionSyntax.Expression as MemberAccessExpressionSyntax)?.Name.Identifier.Text == "ToString")
+			{
+				return;
+			}
+
+			// If nested calls (e.g., Foo(Bar(Meow()))), only trigger the outer violation Bar(Meow())
+			ArgumentSyntax nestedArgumentSyntax = argumentSyntax.Ancestors().OfType<ArgumentSyntax>().FirstOrDefault();
+			if (nestedArgumentSyntax != null)
+			{
+				return;
+			}
+
+			// If we're within a constructor initializer (this(...) or base(...) eg), let it go
+			ConstructorInitializerSyntax constructorInitializerSyntax = argumentSyntax.Ancestors().OfType<ConstructorInitializerSyntax>().FirstOrDefault();
+			if (constructorInitializerSyntax != null)
+			{
+				return;
+			}
+
+			// If the caller is Assert, let it go. (This is debatable, and ideally warrants a configuration option.)
+			MemberAccessExpressionSyntax caller = (argumentSyntax.Parent.Parent as InvocationExpressionSyntax)?.Expression as MemberAccessExpressionSyntax;
+			if (caller?.Expression is IdentifierNameSyntax identifier && identifier.Identifier.ValueText.Contains(@"Assert"))
+			{
+				return;
+			}
+
+			// If the called method is static, let it go to reduce annoyances. E.g., "Times.Once", "Mock.Of<>", "Marshal.Sizeof", etc.
+			// This is debatable, and ideally warrants a configuration option.
+			if (argumentExpressionSyntax.Expression is MemberAccessExpressionSyntax callee)
+			{
+				var symbol = context.SemanticModel.GetSymbolInfo(callee).Symbol;
+				if (symbol.IsStatic)
+				{
+					return;
+				}
+			}
+
+			Diagnostic diagnostic = Diagnostic.Create(Rule, argumentSyntax.GetLocation(), argumentSyntax.ToString());
+			context.ReportDiagnostic(diagnostic);
+		}
+	}
+}
---
+++ Philips.CodeAnalysis.Test/Maintainability/Maintainability/AvoidInvocationAsArgumentTest.cs
@@ -0,0 +1,318 @@
+﻿// © 2021 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using System;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Serialization;
+using Philips.CodeAnalysis.Common;
+using Philips.CodeAnalysis.MaintainabilityAnalyzers.Maintainability;
+
+namespace Philips.CodeAnalysis.Test.Maintainability.Maintainability
+{
+	[TestClass]
+	public class AvoidInvocationAsArgumentAnalyzerTest : CodeFixVerifier
+	{
+
+		[TestMethod]
+		public void AvoidInvocationAsArgumentTest()
+		{
+			string template = @"
+class Foo
+{{
+  public Foo() : base(Do()) {}
+  public Foo(string h) {}
+  public static string Do() {{ return ""hi"";}}
+  public string Moo(string s) {{ return ""hi"";}}
+
+  public void MyTest()
+  {{
+    string.Format(Foo.Do());
+    string.Format(nameof(MyTest));
+    string.Format(5.ToString());
+    string.Format(Moo(Do()));	  // Finding
+    Assert.Format(Moo("""").Format(""""));
+  }}
+}}
+";
+			VerifyCSharpDiagnostic(template, DiagnosticResultHelper.Create(DiagnosticIds.AvoidInvocationAsArgument));
+		}
+
+		[TestMethod]
+		public void AvoidInvocationAsArgumentReturnTest()
+		{
+			string template = @"
+class Foo
+{{
+  public string Do() {{ return ""hi"";}}
+  public string Moo(string s) {{ return ""hi"";}}
+
+  public string MyTest()
+  {{
+    return Moo(Do());
+  }}
+}}
+";
+			VerifyCSharpDiagnostic(template, DiagnosticResultHelper.Create(DiagnosticIds.AvoidInvocationAsArgument));
+		}
+
+		[TestMethod]
+		public void AvoidInvocationAsArgumentFixTest()
+		{
+			string errorContent = @"
+class Foo
+{
+  public string Do() { return ""hi"";}
+  public void Moo(string x) { }
+  public void MyTest()
+  {
+     Moo(Do());
+  }
+}
+";
+			string fixedContent = @"
+class Foo
+{
+  public string Do() { return ""hi"";}
+  public void Moo(string x) { }
+  public void MyTest()
+  {
+     var renameMe = Do();
+     Moo(renameMe);
+  }
+}
+";
+
+			VerifyCSharpDiagnostic(errorContent, DiagnosticResultHelper.Create(DiagnosticIds.AvoidInvocationAsArgument));
+			VerifyCSharpFix(errorContent, fixedContent);
+		}
+
+		[TestMethod]
+		public void AvoidInvocationAsArgumentLocalAssignmentTest()
+		{
+			string errorContent = @"
+class Foo
+{
+  public string Do() { return ""hi"";}
+  public string Moo(string x) { }
+  public void MyTest()
+  {
+     string x = Moo(Do());
+  }
+}
+";
+			string fixedContent = @"
+class Foo
+{
+  public string Do() { return ""hi"";}
+  public string Moo(string x) { }
+  public void MyTest()
+  {
+     var renameMe = Do();
+     string x = Moo(renameMe);
+  }
+}
+";
+
+			VerifyCSharpDiagnostic(errorContent, DiagnosticResultHelper.Create(DiagnosticIds.AvoidInvocationAsArgument));
+			VerifyCSharpFix(errorContent, fixedContent);
+		}
+
+		[TestMethod]
+		public void AvoidInvocationAsArgumentAssignmentTest()
+		{
+			string errorContent = @"
+class Foo
+{
+  public string Do() { return ""hi"";}
+  public string Moo(string x) { }
+  public void MyTest()
+  {
+     string x;
+     x = Moo(Do());
+  }
+}
+";
+			string fixedContent = @"
+class Foo
+{
+  public string Do() { return ""hi"";}
+  public string Moo(string x) { }
+  public void MyTest()
+  {
+     string x;
+     var renameMe = Do();
+     x = Moo(renameMe);
+  }
+}
+";
+
+			VerifyCSharpDiagnostic(errorContent, DiagnosticResultHelper.Create(DiagnosticIds.AvoidInvocationAsArgument));
+			VerifyCSharpFix(errorContent, fixedContent);
+		}
+
+
+		[TestMethod]
+		public void AvoidInvocationAsArgumentFixReturnTest()
+		{
+			string errorContent = @"
+class Foo
+{
+  public string Do() { return ""hi"";}
+  public void Moo(string x) { }
+  public string MyTest()
+  {
+     // Comment
+     return Moo(Do());
+  }
+}
+";
+			string fixedContent = @"
+class Foo
+{
+  public string Do() { return ""hi"";}
+  public void Moo(string x) { }
+  public string MyTest()
+  {
+     // Comment
+     var renameMe = Do();
+     return Moo(renameMe);
+  }
+}
+";
+
+			VerifyCSharpDiagnostic(errorContent, DiagnosticResultHelper.Create(DiagnosticIds.AvoidInvocationAsArgument));
+			VerifyCSharpFix(errorContent, fixedContent);
+		}
+
+		[TestMethod]
+		public void AvoidInvocationAsArgumentFixIfTest()
+		{
+			string errorContent = @"
+class Foo
+{
+  public string Do() { return ""hi"";}
+  public void Moo(string x) { }
+  public void MyTest()
+  {
+     if (true)
+       Moo(Do());
+  }
+}
+";
+			string fixedContent = @"
+class Foo
+{
+  public string Do() { return ""hi"";}
+  public void Moo(string x) { }
+  public void MyTest()
+  {
+     if (true)
+     {
+       var renameMe = Do();
+       Moo(renameMe);
+     }
+  }
+}
+";
+			VerifyCSharpDiagnostic(errorContent, DiagnosticResultHelper.Create(DiagnosticIds.AvoidInvocationAsArgument));
+			VerifyCSharpFix(errorContent, fixedContent);
+		}
+
+		[TestMethod]
+		public void AvoidInvocationAsArgumentFixNewTest()
+		{
+			string errorContent = @"
+class Meow { public Meow(string x) {} }
+class Foo
+{
+  public string Do() { return ""hi"";}
+  public void MyTest()
+  {
+     new Meow(Do());
+  }
+}
+";
+			string fixedContent = @"
+class Meow { public Meow(string x) {} }
+class Foo
+{
+  public string Do() { return ""hi"";}
+  public void MyTest()
+  {
+     var renameMe = Do();
+     new Meow(renameMe);
+  }
+}
+";
+			VerifyCSharpDiagnostic(errorContent, DiagnosticResultHelper.Create(DiagnosticIds.AvoidInvocationAsArgument));
+			VerifyCSharpFix(errorContent, fixedContent);
+		}
+
+		[TestMethod]
+		public void AvoidInvocationAsArgumentFixSwitchTest()
+		{
+			string errorContent = @"
+class Meow { public Meow(string x) {} }
+class Foo
+{
+  public string Do() { return ""hi"";}
+  public void Moo(string x) { }
+  public void MyTest()
+  {
+     int i;
+     switch (i)
+     {
+       case 0: Moo(Do()); break;
+     }
+  }
+}
+";
+			string fixedContent = @"
+class Meow { public Meow(string x) {} }
+class Foo
+{
+  public string Do() { return ""hi"";}
+  public void Moo(string x) { }
+  public void MyTest()
+  {
+     int i;
+     switch (i)
+     {
+       case 0: var renameMe = Do(); Moo(renameMe); break;
+     }
+  }
+}
+";
+			VerifyCSharpDiagnostic(errorContent, DiagnosticResultHelper.Create(DiagnosticIds.AvoidInvocationAsArgument));
+			VerifyCSharpFix(errorContent, fixedContent);
+		}
+
+		[TestMethod]
+		public void AvoidInvocationAsArgumentFixExpressionTest()
+		{
+			string errorContent = @"
+class Meow { public Meow(string x) {} }
+class Foo
+{
+  public string Do() { return ""hi"";}
+  public void Moo(string x) { }
+  public void MyTest() => Moo(Do());
+}
+";
+
+			VerifyCSharpDiagnostic(errorContent, DiagnosticResultHelper.Create(DiagnosticIds.AvoidInvocationAsArgument));
+			//VerifyCSharpFix(errorContent, fixedContent);
+		}
+
+		protected override CodeFixProvider GetCSharpCodeFixProvider()
+		{
+			return new AvoidInvocationAsArgumentCodeFixProvider();
+		}
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new AvoidInvocationAsArgumentAnalyzer();
+        }
+    }
+}
