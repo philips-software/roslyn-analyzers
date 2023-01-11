a5732efab7668c1598bccec4e7eafa09beb58010
Ynse Hoornenborg <ynse.hoornenborg@philips.com>
2023-01-11 23:58:19 +0100

    Align count of + and - operators

---
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/Maintainability/OperatorsCountAnalyzer.cs
@@ -0,0 +1,73 @@
+﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using Philips.CodeAnalysis.Common;
+
+namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.Maintainability
+{
+	/// <summary>
+	/// Diagnostic for inconsistent number of operators in a class.
+	/// </summary>
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class OperatorsCountAnalyzer : DiagnosticAnalyzer
+	{
+		private const string PlusMinusTitle = "Align number of + and - operators.";
+		private const string PlusMinusMessage = "Align number of + and - operators.";
+		private const string PlusMinusDescription = 
+			"A class should have the same number of + as - operators and these shall have the same arguments, in the same order.";
+		private const string Category = Categories.Maintainability;
+
+		private static readonly DiagnosticDescriptor PlusMinusRule =
+			new(
+				Helper.ToDiagnosticId(DiagnosticIds.AlignNumberOfPlusAndMinusOperators),
+				PlusMinusTitle,
+				PlusMinusMessage,
+				Category,
+				DiagnosticSeverity.Error,
+				isEnabledByDefault: true,
+				description: PlusMinusDescription
+			);
+
+		/// <summary>
+		/// <inheritdoc cref="DiagnosticAnalyzer"/>
+		/// </summary>
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
+			ImmutableArray.Create(PlusMinusRule);
+
+		/// <summary>
+		/// <inheritdoc cref="DiagnosticAnalyzer"/>
+		/// </summary>
+		public override void Initialize(AnalysisContext context)
+		{
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+			context.EnableConcurrentExecution();
+			context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.ClassDeclaration);
+		}
+
+		private void Analyze(SyntaxNodeAnalysisContext context)
+		{
+			GeneratedCodeDetector generatedCodeDetector = new();
+			if (generatedCodeDetector.IsGeneratedCode(context))
+			{
+				return;
+			}
+
+			ClassDeclarationSyntax classDeclaration = (ClassDeclarationSyntax)context.Node;
+
+			OperatorsVisitor visitor = new();
+			visitor.Visit(classDeclaration);
+
+			if (visitor.PlusCount != visitor.MinusCount)
+			{
+				var location = classDeclaration.Identifier.GetLocation();
+				context.ReportDiagnostic(Diagnostic.Create(PlusMinusRule, location));
+			}
+		}
+	}
+}
---
+++ Philips.CodeAnalysis.Test/Maintainability/Maintainability/OperatorsCountAnalyzerTest.cs
@@ -0,0 +1,78 @@
+﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Philips.CodeAnalysis.Common;
+using Philips.CodeAnalysis.MaintainabilityAnalyzers.Maintainability;
+
+namespace Philips.CodeAnalysis.Test.Maintainability.Maintainability
+{
+	/// <summary>
+	/// Test class for <see cref="OperatorsCountAnalyzer"/>.
+	/// </summary>
+	[TestClass]
+	public class OperatorsCountAnalyzerTest : DiagnosticVerifier
+	{
+		private const string CorrectNumberOfPlusMinus = @"
+    namespace AssignmentInConditionUnitTests {
+        public class Number {
+			private int n;
+            public static operator +(Number num1, Number num2)
+            {
+                return num1.n + num2.n;
+            }
+            public static operator -(Number num1, Number num2)
+            {
+                return num1.n - num2.n;
+            }
+        }
+    }";
+
+		private const string ViolationNumberOfPlusMinus = @"
+    namespace AssignmentInConditionUnitTests {
+        public class Number {
+			private int n;
+            public static operator +(Number num1, Number num2)
+            {
+                return num1.n + num2.n;
+            }
+        }
+    }";
+
+		/// <summary>
+		/// No diagnostics expected to show up
+		/// </summary>
+		[TestMethod]
+		[DataRow("", DisplayName = "Empty"),
+		 DataRow(CorrectNumberOfPlusMinus, DisplayName = nameof(CorrectNumberOfPlusMinus))]
+		public void WhenTestCodeIsValidNoDiagnosticIsTriggered(string testCode)
+		{
+			VerifyCSharpDiagnostic(testCode);
+		}
+
+		/// <summary>
+		/// Diagnostics expected to show up
+		/// </summary>
+		[TestMethod]
+		[DataRow(ViolationNumberOfPlusMinus, DisplayName = nameof(ViolationNumberOfPlusMinus))]
+		public void WhenDoingAssignmentInsideConditionDiagnosticIsRaised(string testCode) {
+			var expected = DiagnosticResultHelper.Create(DiagnosticIds.AlignNumberOfPlusAndMinusOperators);
+			VerifyCSharpDiagnostic(testCode, expected);
+		}
+
+		/// <summary>
+		/// No diagnostics expected to show up 
+		/// </summary>
+		[TestMethod]
+		[DataRow("File.g", DisplayName = "OutOfScopeSourceFile")]
+		public void WhenSourceFileIsOutOfScopeNoDiagnosticIsTriggered(string filePath)
+		{
+			VerifyCSharpDiagnostic(ViolationNumberOfPlusMinus, filePath);
+		}
+
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() {
+			return new OperatorsCountAnalyzer();
+		}
+	}
+}
