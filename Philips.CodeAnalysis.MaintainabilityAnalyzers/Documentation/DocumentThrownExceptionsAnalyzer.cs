2e0bec3f36180c6f5db193d5e673fb49b44318c7
Ynse Hoornenborg <ynse.hoornenborg@philips.com>
2023-01-19 09:42:44 +0100

    feat: Introduce document thrown exceptions

---
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/Documentation/DocumentThrownExceptionsAnalyzer.cs
@@ -0,0 +1,79 @@
+﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Philips.CodeAnalysis.Common;
+
+namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.Documentation
+{
+	/// <summary>
+	/// Analyzer that checks if the text of the XML code documentation contains a reference to each exception being thrown inside the method or property.
+	/// </summary>
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class DocumentThrownExceptionsAnalyzer : DiagnosticAnalyzer
+	{
+		private const string Title = @"Document thrown exceptions";
+		private const string MessageFormat = @"Document that this method can potentially throw an {0}.";
+		private const string Description = @"Be clear to your callers what exception can be thrown from your method by mentioning each of them in an <exception> element in the documentation of the method.";
+		private const string Category = Categories.Documentation;
+
+		private static readonly DiagnosticDescriptor Rule = new(Helper.ToDiagnosticId(DiagnosticIds.DocumentThrownExceptions), Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: false, description: Description);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+			context.EnableConcurrentExecution();
+			context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.ThrowStatement);
+		}
+
+		private void Analyze(SyntaxNodeAnalysisContext context)
+		{
+			var throwStatement = (ThrowStatementSyntax)context.Node;
+			if (throwStatement.Expression is not ObjectCreationExpressionSyntax exceptionType)
+			{
+				return;
+			}
+
+			var thrownExceptionName = (exceptionType.Type as IdentifierNameSyntax)?.Identifier.Text;
+			if (string.IsNullOrEmpty(thrownExceptionName))
+			{
+				return;
+			}
+
+			var methodDeclaration = throwStatement.Ancestors().OfType<MethodDeclarationSyntax>().FirstOrDefault();
+			if (methodDeclaration == null)
+			{
+				return;
+			}
+
+			var mentionedExceptions = methodDeclaration.GetLeadingTrivia()
+				.Select(i => i.GetStructure())
+				.OfType<DocumentationCommentTriviaSyntax>()
+				.SelectMany(n => n.ChildNodes().OfType<XmlElementSyntax>())
+				.Where(IsExceptionElement)
+				.Select(GetCrefAttributeValue);
+			if (!mentionedExceptions.Contains(thrownExceptionName))
+			{
+				Diagnostic diagnostic = Diagnostic.Create(Rule, exceptionType.GetLocation(), thrownExceptionName);
+				context.ReportDiagnostic(diagnostic);
+			}
+		}
+
+		private static bool IsExceptionElement(XmlElementSyntax element)
+		{
+			return element.StartTag.Name.LocalName.Text == "exception";
+		}
+
+		private static string GetCrefAttributeValue(XmlElementSyntax element)
+		{
+			return element.StartTag.Attributes.OfType<XmlCrefAttributeSyntax>().Select(cref => cref.Cref.ToString()).FirstOrDefault();
+		}
+
+	}
+}
---
+++ Philips.CodeAnalysis.Test/Maintainability/Documentation/DocumentThrownExceptionsAnalyzerTest.cs
@@ -0,0 +1,84 @@
+﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Philips.CodeAnalysis.Common;
+using Philips.CodeAnalysis.MaintainabilityAnalyzers.Documentation;
+
+namespace Philips.CodeAnalysis.Test.Maintainability.Documentation
+{
+	[TestClass]
+	public class DocumentThrownExceptionsAnalyzerTest : DiagnosticVerifier
+	{
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+		{
+			return new DocumentThrownExceptionsAnalyzer();
+		}
+
+		private const string CorrectNoThrow = @"
+public class Foo
+{
+    /// <summary> Helpful text. </summary>
+    public void MethodA()
+    {
+    }
+}
+";
+
+		private const string CorrectWithThrow = @"
+public class Foo
+{
+    /// <summary> Helpful text. </summary>
+    /// <exception cref=""ArgumentException"">
+    public void MethodA()
+    {
+        throw new ArgumentException(""Error"");
+    }
+}
+";
+
+		private const string WrongNoDoc = @"
+public class Foo
+{
+    /// <summary> Helpful text. </summary>
+    public void MethodA()
+    {
+        throw new ArgumentException(""Error"");
+    }
+}
+";
+
+		private const string WrongType = @"
+public class Foo
+{
+    /// <summary> Helpful text. </summary>
+    /// <exception cref=""InvalidOperationException"">
+    public void MethodA()
+    {
+        throw new ArgumentException(""Error"");
+    }
+}
+";
+
+		[DataTestMethod]
+		[DataRow(CorrectNoThrow, DisplayName = nameof(CorrectNoThrow)),
+		 DataRow(CorrectWithThrow, DisplayName = nameof(CorrectWithThrow))]
+		public void CorrectCodeShouldNotTriggerAnyDiagnostics(string testCode)
+		{
+			VerifyCSharpDiagnostic(testCode);
+		}
+
+		[DataTestMethod]
+		[DataRow(WrongNoDoc, DisplayName = nameof(WrongNoDoc)),
+		 DataRow(WrongType, DisplayName = nameof(WrongType))]
+		public void MissingOrWrongDocumentationShouldTriggerDiagnostic(string testCode)
+		{
+			VerifyCSharpDiagnostic(testCode, DiagnosticResultHelper.Create(DiagnosticIds.DocumentThrownExceptions));
+		}
+	}
+}
