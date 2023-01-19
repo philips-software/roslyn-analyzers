f130b0bd67fde36ba10e2818f4b19f08a1834dcb
Collamore, Brian <brian.collamore@philips.com>
2023-01-18 21:22:33 -0500

    feat: enhance PH2006- support folder in namespace

--- Philips.CodeAnalysis.MaintainabilityAnalyzers/Naming/NamespaceMatchAssemblyAnalyzer.cs
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/Naming/NamespaceMatchAssemblyAnalyzer.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Immutable;
+using System.IO;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -20,16 +21,31 @@ namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.Naming
 
 		private static readonly DiagnosticDescriptor Rule = new(Helper.ToDiagnosticId(DiagnosticIds.NamespaceMatchAssembly), Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
 
+		private readonly GeneratedCodeAnalysisFlags _generatedCodeFlags;
+		private AdditionalFilesHelper _additionalFilesHelper;
+		private bool _folderInNamespace;
+		private bool _configInitialized;
+
+		public NamespaceMatchAssemblyAnalyzer()
+			: this(GeneratedCodeAnalysisFlags.None, null)
+		{ }
+
+		public NamespaceMatchAssemblyAnalyzer(GeneratedCodeAnalysisFlags generatedCodeFlags, AdditionalFilesHelper additionalFilesHelper)
+		{
+			_generatedCodeFlags = generatedCodeFlags;
+			_additionalFilesHelper = additionalFilesHelper;
+		}
+
 		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
 		public override void Initialize(AnalysisContext context)
 		{
-			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+			context.ConfigureGeneratedCodeAnalysis(_generatedCodeFlags);
 			context.EnableConcurrentExecution();
 			context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.NamespaceDeclaration);
 		}
 
-		private static void Analyze(SyntaxNodeAnalysisContext context)
+		private void Analyze(SyntaxNodeAnalysisContext context)
 		{
 			GeneratedCodeDetector generatedCodeDetector = new();
 			if (generatedCodeDetector.IsGeneratedCode(context))
@@ -41,10 +57,23 @@ namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.Naming
 			string myNamespace = namespaceDeclaration.Name.ToString();
 			string myFilePath = context.Node.SyntaxTree.FilePath;
 
-			// Check file path
-			if (IsFilePartOfPath(myNamespace, myFilePath))
+			InitializeConfiguration(context);
+
+			if (_folderInNamespace)
 			{
-				return;
+				// Does the namespace exactly match the trailing folders?
+				if (DoesFilePathEndWithNamespace(myNamespace, myFilePath))
+				{
+					return;
+				}
+			}
+			else
+			{
+				// Does the namespace exactly match one of the folders in the path?
+				if (IsNamespacePartOfPath(myNamespace, myFilePath))
+				{
+					return;
+				}
 			}
 
 			// TODO: Check assembly name, see issue #174
@@ -53,19 +82,35 @@ namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.Naming
 			context.ReportDiagnostic(diagnostic);
 		}
 
-		private static bool IsFilePartOfPath(string ns, string path)
+		private bool IsNamespacePartOfPath(string namespact, string path)
 		{
-			string[] nodes = path.Split('/', '\\');
-
+			string[] nodes = path.Split(Path.DirectorySeparatorChar);
 			for (int i = nodes.Length - 2; i > 0; i--)  // Exclude file.cs (i.e., the end) and the drive (i.e., the start).  Start from back to succeed quickly.
 			{
-				if (string.Equals(nodes[i], ns, StringComparison.OrdinalIgnoreCase))
+				if (string.Equals(nodes[i], namespact, StringComparison.OrdinalIgnoreCase))
 				{
 					return true;
 				}
 			}
-
 			return false;
+		}
+
+		private bool DoesFilePathEndWithNamespace(string ns, string path)
+		{
+			string folder = Path.GetDirectoryName(path);
+			string allowedNamespace = folder.Replace(Path.DirectorySeparatorChar, '.');
+			return allowedNamespace.EndsWith(ns, StringComparison.OrdinalIgnoreCase);
+		}
+
+		private void InitializeConfiguration(SyntaxNodeAnalysisContext context)
+		{
+			if (!_configInitialized)
+			{
+				_additionalFilesHelper ??= new AdditionalFilesHelper(context.Options, context.Compilation);
+				string folderInNamespace = _additionalFilesHelper.GetValueFromEditorConfig(Rule.Id, @"folder_in_namespace");
+				_ = bool.TryParse(folderInNamespace, out _folderInNamespace);
+				_configInitialized = true;
+			}
 		}
 	}
 }
--- Philips.CodeAnalysis.Test/Maintainability/Naming/NamespaceMatchAssemblyAnalyzerTest.cs
+++ Philips.CodeAnalysis.Test/Maintainability/Naming/NamespaceMatchAssemblyAnalyzerTest.cs
@@ -1,22 +1,22 @@
 ﻿// © 2022 Koninklijke Philips N.V. See License.md in the project root for license information.
 
 using System;
+using System.Collections.Immutable;
+using System.IO;
 using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using Philips.CodeAnalysis.Common;
 using Philips.CodeAnalysis.MaintainabilityAnalyzers.Naming;
 
 namespace Philips.CodeAnalysis.Test.Maintainability.Naming
 {
 	[TestClass]
-	public class NamespaceMatchAssemblyAnalyzerTest : DiagnosticVerifier
+	public class NamespaceMatchAssemblyAnalyzerUseFolderTest : DiagnosticVerifier
 	{
-
-		#region Non-Public Data Members
-
-		private const string ClassString = @"
+		public const string ClassString = @"
 			using System;
 			using System.Globalization;
 			namespace {0}
@@ -30,31 +30,28 @@ namespace Philips.CodeAnalysis.Test.Maintainability.Naming
 			}}
 			";
 
-		#endregion
-
-		#region Non-Public Properties/Methods
-		private DiagnosticResultLocation GetBaseDiagnosticLocation(string path, int rowOffset = 0, int columnOffset = 0)
+		public static DiagnosticResultLocation GetBaseDiagnosticLocation(string path, int rowOffset = 0, int columnOffset = 0)
 		{
 			return new DiagnosticResultLocation(path + ".cs", 4 + rowOffset, 14 + columnOffset);
 		}
 
 		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
 		{
-			return new NamespaceMatchAssemblyAnalyzer();
+			Mock<AdditionalFilesHelper> _mockAdditionalFilesHelper = new(new AnalyzerOptions(ImmutableArray.Create<AdditionalText>()), null);
+			_mockAdditionalFilesHelper.Setup(c => c.GetValueFromEditorConfig(It.IsAny<string>(), It.IsAny<string>())).Returns("true");
+			return new NamespaceMatchAssemblyAnalyzer(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics, _mockAdditionalFilesHelper.Object);
 		}
 
-		#endregion
-
-
-		#region Test Methods
 		[DataTestMethod]
-		[DataRow("Philips.Test", "C:\\development\\Philips.Production\\code\\MyTest")]
-		[DataRow("Philips.CodeAnalysis.Test", "C:\\Philips.CodeAnalysis.Production\\MyAnalyzer")]
-		[DataRow("Philips.CodeAnalysis.Test", "C:\\Philips.CodeAnalysis.TestFramework\\MyHelper")]
-		public void ReportIncorrectNamespaceMatch(string prefix, string path)
+		[DataRow("Philips.Test", "C:\\development\\Philips.Production\\code\\MyTest.cs")]
+		[DataRow("Philips.CodeAnalysis.Test", "C:\\Philips.CodeAnalysis.Production\\MyAnalyzer.cs")]
+		[DataRow("Philips.CodeAnalysis.Test", "C:\\Philips.CodeAnalysis.TestFramework\\MyHelper.cs")]
+		[DataRow("Philips.Test", "C:\\development\\Philips.Test\\code\\MyTest.cs", DisplayName="Namespace Match, Folder Does not")]
+		[DataRow("Philips.CodeAnalysis.Test", "C:\\Philips.CodeAnalysis.Test\\src\\MyTest.cs", DisplayName = "Namespace Match, Folder Does not")]
+		public void ReportIncorrectNamespaceMatch(string ns, string path)
 		{
-
-			string code = string.Format(ClassString, prefix);
+			path = path.Replace('\\', Path.DirectorySeparatorChar);
+			string code = string.Format(ClassString, ns);
 			DiagnosticResult expected = new()
 			{
 				Id = Helper.ToDiagnosticId(DiagnosticIds.NamespaceMatchAssembly),
@@ -70,14 +67,61 @@ namespace Philips.CodeAnalysis.Test.Maintainability.Naming
 		}
 
 		[DataTestMethod]
-		[DataRow("Philips.Test", "C:\\development\\Philips.Test\\code\\MyTest")]
-		[DataRow("Philips.CodeAnalysis.Test", "C:\\Philips.CodeAnalysis.Test\\MyTest")]
-		public void DoNotReportANamespaceMatchError(string ns, string path)
+		[DataRow("Philips.CodeAnalysis.Test.Maintainability", "C:\\Philips.CodeAnalysis.Test\\Maintainability\\blah.cs", DisplayName = "Folder Match Included 1")]
+		[DataRow("Philips.CodeAnalysis.Test.Maintainability", "C:\\repos\\Philips.CodeAnalysis.Test\\Maintainability\\blah.cs", DisplayName = "Folder Match Included 2")]
+		[DataRow("Philips.CodeAnalysis.Test.Maintainability.Foo", "C:\\repos\\Philips.CodeAnalysis.Test\\Maintainability\\Foo\\blah.cs", DisplayName = "Folder Match Included 2")]
+		public void DoNotReportANamespaceSupersetMatch(string ns, string path)
 		{
+			path = path.Replace('\\', Path.DirectorySeparatorChar);
 			string code = string.Format(ClassString, ns);
 			VerifyCSharpDiagnostic(code, path, Array.Empty<DiagnosticResult>());
 		}
-
-		#endregion
 	}
+
+	[TestClass]
+	public class NamespaceMatchAssemblyAnalyzerNoFolderTest : DiagnosticVerifier
+	{
+		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+		{
+			Mock<AdditionalFilesHelper> _mockAdditionalFilesHelper = new(new AnalyzerOptions(ImmutableArray.Create<AdditionalText>()), null);
+			_mockAdditionalFilesHelper.Setup(c => c.GetValueFromEditorConfig(It.IsAny<string>(), It.IsAny<string>())).Returns("false");
+			return new NamespaceMatchAssemblyAnalyzer(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics, _mockAdditionalFilesHelper.Object);
+		}
+
+		[DataTestMethod]
+		[DataRow("Philips.Test", "C:\\development\\Philips.Production\\code\\MyTest.cs")]
+		[DataRow("Philips.CodeAnalysis.Test", "C:\\Philips.CodeAnalysis.Production\\MyAnalyzer.cs")]
+		[DataRow("Philips.CodeAnalysis.Test", "C:\\Philips.CodeAnalysis.TestFramework\\MyHelper.cs")]
+		[DataRow("Philips.CodeAnalysis.Test.Maintainability", "C:\\Philips.CodeAnalysis.Test\\Maintainability\\blah.cs", DisplayName = "Folder Match Included 1")]
+		[DataRow("Philips.CodeAnalysis.Test.Maintainability", "C:\\repos\\Philips.CodeAnalysis.Test\\Maintainability\\blah.cs", DisplayName = "Folder Match Included 2")]
+		[DataRow("Philips.CodeAnalysis.Test.Maintainability.Foo", "C:\\repos\\Philips.CodeAnalysis.Test\\Maintainability\\Foo\\blah.cs", DisplayName = "Folder Match Included 2")]
+		public void ReportIncorrectNamespaceMatch(string ns, string path)
+		{
+			path = path.Replace('\\', Path.DirectorySeparatorChar);
+			string code = string.Format(NamespaceMatchAssemblyAnalyzerUseFolderTest.ClassString, ns);
+			DiagnosticResult expected = new()
+			{
+				Id = Helper.ToDiagnosticId(DiagnosticIds.NamespaceMatchAssembly),
+				Message = new Regex(".+ "),
+				Severity = DiagnosticSeverity.Error,
+				Locations = new[]
+				{
+					NamespaceMatchAssemblyAnalyzerUseFolderTest.GetBaseDiagnosticLocation(path, 0,0)
+				}
+			};
+
+			VerifyCSharpDiagnostic(code, path, expected);
+		}
+
+		[DataTestMethod]
+		[DataRow("Philips.Test", "C:\\development\\Philips.Test\\code\\MyTest.cs", DisplayName = "Namespace Match, Folder Does not")]
+		[DataRow("Philips.CodeAnalysis.Test", "C:\\Philips.CodeAnalysis.Test\\src\\MyTest.cs", DisplayName = "Namespace Match, Folder Does not")]
+		public void DoNotReportANamespaceSupersetMatch(string ns, string path)
+		{
+			path = path.Replace('\\', Path.DirectorySeparatorChar);
+			string code = string.Format(NamespaceMatchAssemblyAnalyzerUseFolderTest.ClassString, ns);
+			VerifyCSharpDiagnostic(code, path, Array.Empty<DiagnosticResult>());
+		}
+	}
+
 }
