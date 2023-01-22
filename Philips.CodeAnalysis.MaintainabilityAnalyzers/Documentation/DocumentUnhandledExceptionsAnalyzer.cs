cece2a7dc210e27f2e7090219c4ad721c605f0f9
Ynse Hoornenborg <ynse.hoornenborg@philips.com>
2023-01-22 14:17:11 +0100

    First working version

---
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/Documentation/DocumentUnhandledExceptionsAnalyzer.cs
@@ -0,0 +1,225 @@
+﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Philips.CodeAnalysis.Common;
+
+namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.Documentation
+{
+	/// <summary>
+	/// Analyzer that checks if the text of the XML code documentation contains a reference to each exception being unhandled in the method or property.
+	/// </summary>
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class DocumentUnhandledExceptionsAnalyzer : DiagnosticAnalyzer
+	{
+		private const string Title = @"Document unhandled exceptions";
+		private const string MessageFormat = @"Document that this method can throw from {0} the following exceptions: {1}.";
+		private const string Description = @"Be clear to your callers what exception can be thrown from your method by mentioning each of them in an <exception> element in the documentation of the method.";
+		private const string Category = Categories.Documentation;
+
+		private static class WellKnownExceptions
+		{
+			public const string Exception = "System.Exception";
+			public const string ArgumentException = "System.ArgumentException";
+			public const string ArgumentNullException = "System.ArgumentNullException";
+			public const string ArgumentOutOfRangeException = "System.ArgumentOutOfRangeException";
+			public const string DirectoryNotFoundException = "System.IO.DirectoryNotFoundException";
+			public const string IoException = "System.IO.IOException";
+			public const string NotSupportedException = "System.NotSupportedException";
+			public const string PathTooLongException = "System.IO.PathTooLongException";
+			public const string SecurityException = "System.SecurityException";
+			public const string UnauthorizedException = "System.UnauthorizedException";
+		}
+
+		private static readonly DiagnosticDescriptor Rule =
+			new(Helper.ToDiagnosticId(DiagnosticIds.DocumentUnhandledExceptions), Title, MessageFormat, Category,
+				DiagnosticSeverity.Error, isEnabledByDefault: false, description: Description);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+		private static readonly Regex DocumentationRegex = new("exception\\scref=\\\"(.*)\\\"", RegexOptions.Compiled, TimeSpan.FromSeconds(1));
+
+		[SuppressMessage("", "IDE0090", Justification = "When type is removed the literal type cannot be deduced")]
+		private static readonly Dictionary<string, string[]> ExceptionsMap = new Dictionary<string, string[]>
+		{
+			{
+				"System.IO.Directory.CreateDirectory",
+				new[]
+				{
+					WellKnownExceptions.IoException, WellKnownExceptions.UnauthorizedException, WellKnownExceptions.ArgumentException, WellKnownExceptions.ArgumentNullException, WellKnownExceptions.PathTooLongException, WellKnownExceptions.DirectoryNotFoundException, WellKnownExceptions.NotSupportedException
+				}
+			},
+			{
+				"System.IO.Directory.Delete",
+				new[]
+				{
+					WellKnownExceptions.IoException, WellKnownExceptions.UnauthorizedException, WellKnownExceptions.ArgumentException, WellKnownExceptions.ArgumentNullException, WellKnownExceptions.PathTooLongException, WellKnownExceptions.DirectoryNotFoundException
+				}
+			},
+			{
+				"System.IO.Directory.GetFiles",
+				new[]
+				{
+					WellKnownExceptions.IoException, WellKnownExceptions.ArgumentOutOfRangeException, WellKnownExceptions.ArgumentException, WellKnownExceptions.ArgumentNullException, WellKnownExceptions.PathTooLongException, WellKnownExceptions.DirectoryNotFoundException, WellKnownExceptions.SecurityException
+				}
+			},
+			{
+				"System.IO.Directory.GetDirectories",
+				new[]
+				{
+					WellKnownExceptions.IoException, WellKnownExceptions.ArgumentException, WellKnownExceptions.ArgumentNullException, WellKnownExceptions.PathTooLongException, WellKnownExceptions.DirectoryNotFoundException
+				}
+			},
+			{
+				"System.IO.Directory.EnumerateFiles",
+				new[]
+				{
+					WellKnownExceptions.IoException, WellKnownExceptions.ArgumentOutOfRangeException, WellKnownExceptions.ArgumentException, WellKnownExceptions.ArgumentNullException, WellKnownExceptions.PathTooLongException, WellKnownExceptions.DirectoryNotFoundException, WellKnownExceptions.SecurityException
+				}
+			},
+			{
+				"System.IO.Directory.EnumerateDirectories",
+				new[]
+				{
+					WellKnownExceptions.IoException, WellKnownExceptions.ArgumentOutOfRangeException, WellKnownExceptions.ArgumentException, WellKnownExceptions.ArgumentNullException, WellKnownExceptions.PathTooLongException, WellKnownExceptions.DirectoryNotFoundException, WellKnownExceptions.SecurityException
+				}
+			},
+			{
+				"System.IO.Directory.Move",
+				new[]
+				{
+					WellKnownExceptions.IoException, WellKnownExceptions.ArgumentException, WellKnownExceptions.ArgumentNullException, WellKnownExceptions.PathTooLongException, WellKnownExceptions.DirectoryNotFoundException
+				}
+			},
+		};
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+			context.EnableConcurrentExecution();
+			context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.MethodDeclaration);
+		}
+
+		private static void Analyze(SyntaxNodeAnalysisContext context)
+		{
+			var method = (MethodDeclarationSyntax)context.Node;
+			
+			// TODO: Read PDB as done in: https://github.com/microsoft/peeker
+
+			var invocations = method.DescendantNodes().OfType<InvocationExpressionSyntax>();
+			List<string> unhandledExceptions = new();
+			foreach (var invocation in invocations)
+			{
+				var newExceptions = GetUnhandledExceptions(context, invocation);
+				if (newExceptions.Any())
+				{
+					unhandledExceptions.AddRange(newExceptions);
+				}
+			}
+
+			// List the documented exception types.
+			var documentedExceptions = method.GetLeadingTrivia()
+				.Select(i => i.GetStructure())
+				.OfType<DocumentationCommentTriviaSyntax>()
+				.SelectMany(n => n.ChildNodes().OfType<XmlElementSyntax>())
+				.Where(IsExceptionElement)
+				.Select(cref => GetCrefAttributeValue(context, cref));
+			var comparer = new NamespaceIgnoringComparer();
+			var remainingExceptions = unhandledExceptions.Where(ex => documentedExceptions.All(doc => comparer.Compare(ex, doc) != 0));
+			if (remainingExceptions.Any())
+			{
+				var loc = method.Identifier.GetLocation();
+				var methodName = method.Identifier.Text;
+				string unhandledStr = string.Join(",", remainingExceptions);
+				Diagnostic diagnostic = Diagnostic.Create(Rule, loc, methodName, unhandledStr);
+				context.ReportDiagnostic(diagnostic);
+			}
+		}
+
+		private static bool IsExceptionElement(XmlElementSyntax element)
+		{
+			return element.StartTag.Name.LocalName.Text == "exception";
+		}
+
+		private static string GetCrefAttributeValue(SyntaxNodeAnalysisContext context, XmlElementSyntax element)
+		{
+			return element.StartTag.Attributes.OfType<XmlCrefAttributeSyntax>().Select(cref => cref.Cref.ToString())
+				.FirstOrDefault();
+		}
+
+		private static IEnumerable<string> GetUnhandledExceptions(SyntaxNodeAnalysisContext context,
+			InvocationExpressionSyntax invocation)
+		{
+			var expectedExceptions = Array.Empty<string>();
+			var invokedSymbol = context.SemanticModel.GetSymbolInfo(invocation).Symbol;
+			if (invokedSymbol is IMethodSymbol or IPropertySymbol)
+			{
+				var invokedName = GetFullName(invokedSymbol);
+				if (!ExceptionsMap.TryGetValue(invokedName, out expectedExceptions))
+				{
+					var documentation = invokedSymbol.GetDocumentationCommentXml();
+					expectedExceptions = ParseFromDocumentation(context, invocation, documentation);
+				}
+			}
+
+			IEnumerable<string> unhandledExceptions = expectedExceptions;
+			var tryStatements = invocation.Ancestors().OfType<TryStatementSyntax>();
+			foreach (var tryStatement in tryStatements)
+			{
+				var handledExceptionTypes = tryStatement.Catches.Select(cat => GetFullName(cat.Declaration?.Type));
+				unhandledExceptions = handledExceptionTypes.Any(ex => ex == WellKnownExceptions.Exception) ? Array.Empty<string>() : unhandledExceptions.Except(handledExceptionTypes);
+			}
+
+			return unhandledExceptions;
+		}
+
+		private static string GetFullName(TypeSyntax typeSyntax)
+		{
+			if (typeSyntax is SimpleNameSyntax simpleNameSyntax)
+			{
+				return simpleNameSyntax.Identifier.Text;
+			}
+
+			if (typeSyntax is QualifiedNameSyntax qualifiedNameSyntax)
+			{
+				string left = GetFullName(qualifiedNameSyntax.Left);
+				string right = qualifiedNameSyntax.Right.Identifier.Text;
+				return $"{left}.{right}";
+			}
+
+			return string.Empty;
+		}
+
+		private static string GetFullName(ISymbol symbol)
+		{
+			List<string> namespaces = new();
+			var ns = symbol.ContainingNamespace;
+			while(ns != null && !string.IsNullOrEmpty(ns.Name))
+			{
+				namespaces.Add(ns.Name);
+				ns = ns.ContainingNamespace;
+			}
+			namespaces.Reverse();
+			return $"{string.Join(".", namespaces)}.{symbol.ContainingType.Name}.{symbol.Name}";
+		}
+
+		private static string[] ParseFromDocumentation(SyntaxNodeAnalysisContext context, SyntaxNode node, string documentation)
+		{
+			var matches = DocumentationRegex.Matches(documentation);
+			var results = new string[matches.Count];
+			for (int i = 0; i < results.Length; i++)
+			{
+				results[i] = matches[i].Captures[0].Value.Trim('!', ':');
+			}
+			return results;
+		}
+	}
+}
---
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/Documentation/NamespaceIgnoringComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.Documentation
+{
+	internal class NamespaceIgnoringComparer : IEqualityComparer<string>
+	{
+		public int Compare(string x, string y)
+		{
+			int dotX = Math.Max(x.LastIndexOf(".", StringComparison.Ordinal) + 1, 0);
+			string comparableX = x.Substring(dotX);
+			int dotY = Math.Max(y.LastIndexOf(".", StringComparison.Ordinal) + 1, 0);
+			string comparableY = y.Substring( dotY);
+			return StringComparer.Ordinal.Compare(comparableX, comparableY);
+		}
+
+		public bool Equals(string x, string y)
+		{
+			return Compare(x, y) == 0;
+		}
+
+		public int GetHashCode(string obj)
+		{
+			return obj.GetHashCode();
+		}
+	}
+}
