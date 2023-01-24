8fa10753c57ba7401da7e4a731a883b99b0d7cfd
Brian Collamore <57269455+bcollamore@users.noreply.github.com>
2023-01-24 08:56:17 -0500

    test: CodeFixVerifier: verify the change fixed the diagnostic (#254)

---
+++ Philips.CodeAnalysis.Test/Common/HelperTest.cs
@@ -0,0 +1,33 @@
+﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Philips.CodeAnalysis.Common;
+
+namespace Philips.CodeAnalysis.Test.Common
+{
+	[TestClass]
+	public class HelperTest
+	{
+		private Diagnostic Make(string id)
+		{
+			var descriptor = new DiagnosticDescriptor(id, string.Empty, string.Empty, string.Empty, DiagnosticSeverity.Error, false);
+			return Diagnostic.Create(descriptor, null);
+		}
+
+		[TestMethod]
+		public void ToPrettyListTest()
+		{
+			Diagnostic diagnostic1 = Make("PH1000");
+			Diagnostic diagnostic2 = Make("PH2000");
+			Diagnostic diagnostic3 = Make("PH3000");
+
+			Assert.AreEqual("", Helper.ToPrettyList(Array.Empty<Diagnostic>()));
+			Assert.AreEqual("PH1000", Helper.ToPrettyList(new Diagnostic[] { diagnostic1 }));
+			Assert.AreEqual("PH1000, PH2000", Helper.ToPrettyList(new Diagnostic[] { diagnostic1, diagnostic2 }));
+			Assert.AreEqual("PH1000, PH2000, PH3000", Helper.ToPrettyList(new Diagnostic[] { diagnostic1, diagnostic2, diagnostic3 }));
+		}
+	}
+}
