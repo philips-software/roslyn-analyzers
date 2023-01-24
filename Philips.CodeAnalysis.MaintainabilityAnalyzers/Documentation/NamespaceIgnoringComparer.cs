56261e15e034c5fe6fca115832d73fed353e2105
Ynse Hoornenborg <ynse.hoornenborg@philips.com>
2023-01-24 11:23:52 +0100

    Copyright statement

--- Philips.CodeAnalysis.MaintainabilityAnalyzers/Documentation/NamespaceIgnoringComparer.cs
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/Documentation/NamespaceIgnoringComparer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 
 namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.Documentation
