b047fcf8daedf3efca7e9ba70dc443ed65e63085
Ynse Hoornenborg <ynse.hoornenborg@philips.com>
2023-01-27 12:02:56 +0100

    feat: Align filename and class name

---
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/Naming/AlignFilenameAndClassNameAnalyzer.cs
@@ -0,0 +1,91 @@
+﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Philips.CodeAnalysis.Common;
+
+namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.Naming
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class AlignFilenameAndClassNameAnalyzer : DiagnosticAnalyzer
+	{
+		private const string Title = @"Align filename and class name";
+		private const string MessageFormat = @"Name the file {0}.cs to align with the name of the {1} it contains.";
+		private const string Description = @"Name the file after the class, struct or enum it contains";
+		private const string Category = Categories.Naming;
+
+		private static readonly DiagnosticDescriptor Rule = new(Helper.ToDiagnosticId(DiagnosticIds.AlignFilenameAndClassName),
+			Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true,
+			description: Description);
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+		public override void Initialize(AnalysisContext context)
+		{
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+			context.EnableConcurrentExecution();
+			context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.ClassDeclaration);
+			context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.StructDeclaration);
+			context.RegisterSyntaxNodeAction(AnalyzeEnum, SyntaxKind.EnumDeclaration);
+		}
+
+		private void Analyze(SyntaxNodeAnalysisContext context)
+		{
+			GeneratedCodeDetector generatedCodeDetector = new();
+			if (generatedCodeDetector.IsGeneratedCode(context))
+			{
+				return;
+			}
+
+			TypeDeclarationSyntax typeDeclaration = (TypeDeclarationSyntax)context.Node;
+			string typeKind = null;
+			if(typeDeclaration is ClassDeclarationSyntax)
+			{
+				typeKind = "class";
+			}
+			else if(typeDeclaration is StructDeclarationSyntax)
+			{
+				typeKind = "struct";
+			}
+
+			if (string.IsNullOrEmpty(typeKind))
+			{
+				return;
+			}
+			Check(context, typeDeclaration.Identifier, typeKind);
+		}
+
+		private void AnalyzeEnum(SyntaxNodeAnalysisContext context)
+		{
+			GeneratedCodeDetector generatedCodeDetector = new();
+			if(generatedCodeDetector.IsGeneratedCode(context))
+			{
+				return;
+			}
+
+			EnumDeclarationSyntax enumDeclaration = (EnumDeclarationSyntax)context.Node;
+			Check(context, enumDeclaration.Identifier, "enum");
+		}
+
+		private static void Check(SyntaxNodeAnalysisContext context, SyntaxToken identifier, string typeKind)
+		{
+			var filename = Path.GetFileNameWithoutExtension(context.Node.SyntaxTree.FilePath);
+			int indexOfDot = filename.LastIndexOf('.');
+			if (indexOfDot != -1)
+			{
+				filename = filename.Substring(0,indexOfDot);
+			}
+			if (StringComparer.OrdinalIgnoreCase.Compare(identifier.Text, filename) != 0)
+			{
+				var location = identifier.GetLocation();
+				Diagnostic diagnostic = Diagnostic.Create(Rule, location, identifier.Text, typeKind);
+				context.ReportDiagnostic(diagnostic);
+			}
+		}
+	}
+}
---
+++ Philips.CodeAnalysis.Test/Maintainability/Naming/AlignFilenameAndClassNameAnalyzerTest.cs
@@ -0,0 +1,54 @@
+﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Philips.CodeAnalysis.Common;
+using Philips.CodeAnalysis.MaintainabilityAnalyzers.Naming;
+
+namespace Philips.CodeAnalysis.Test.Maintainability.Naming
+{
+	/// <summary>
+	/// Test class for <see cref="AlignFilenameAndClassNameAnalyzer"/>.
+	/// </summary>
+	[TestClass]
+	public class AlignFilenameAndClassNameAnalyzerTest : DiagnosticVerifier
+	{
+		private const string SourceCodeTemplate = @"
+namespace AlignFilenameAndClassName {{
+    {0} Program {{
+    }}
+}}";
+
+		/// <summary>
+		/// No diagnostics expected to show up
+		/// </summary>
+		[TestMethod]
+		[DataRow("class", "Program"),
+		 DataRow("class", "Program.Part"),
+		 DataRow("struct", "Program"),
+		 DataRow("enum", "Program")]
+		public void WhenTestCodeIsValidNoDiagnosticIsTriggered(string typeKind, string filePath)
+		{
+			VerifyDiagnostic(string.Format(SourceCodeTemplate, typeKind), filePath);
+		}
+
+		/// <summary>
+		/// Diagnostics should show up hare.
+		/// </summary>
+		[TestMethod]
+		[DataRow("class", "Program2"),
+		 DataRow("struct", "SomethingElse"),
+		 DataRow("enum", "Prog")]
+		public void WhenPathIsTooLongDiagnosticIsRaised(string typeKind, string filePath)
+		{
+			var expected = DiagnosticResultHelper.Create(DiagnosticIds.AlignFilenameAndClassName);
+			VerifyDiagnostic(string.Format(SourceCodeTemplate, typeKind), filePath, expected);
+		}
+
+		protected override DiagnosticAnalyzer GetDiagnosticAnalyzer()
+		{
+			return new AlignFilenameAndClassNameAnalyzer();
+		}
+	}
+}
