254fd310fa67ce63ee0bed46ba0037c48d02b448
Ynse Hoornenborg <ynse.hoornenborg@philips.com>
2023-01-27 17:43:17 +0100

    feat: Finalizer allows dispose but no other calls (#284)
    Co-authored-by: Brian Collamore <57269455+bcollamore@users.noreply.github.com>

--- Philips.CodeAnalysis.MaintainabilityAnalyzers/RuntimeFailure/AvoidImplementingFinalizersAnalyzer.cs
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/RuntimeFailure/AvoidImplementingFinalizersAnalyzer.cs
@@ -1,6 +1,8 @@
 ﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
 
+using System;
 using System.Collections.Immutable;
+using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -12,9 +14,9 @@ namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.RuntimeFailure
 	[DiagnosticAnalyzer(LanguageNames.CSharp)]
 	public class AvoidImplementingFinalizersAnalyzer : DiagnosticAnalyzer
 	{
-		private const string Title = @"Don't implement a finalizer";
-		private const string MessageFormat = @"Don't implement a finalizer, use Dispose instead.";
-		private const string Description = @"Don't implement a finalizer, use Dispose instead.";
+		private const string Title = @"Avoid implementing a finalizer";
+		private const string MessageFormat = @"Avoid implement a finalizer, use Dispose instead.";
+		private const string Description = @"Avoid implement a finalizer, use Dispose instead. If the class has unmanaged fields, finalizers are allowed if they only call Dispose.";
 		private const string Category = Categories.Maintainability;
 
 		private static readonly DiagnosticDescriptor Rule = new(Helper.ToDiagnosticId(DiagnosticIds.AvoidImplementingFinalizers),
@@ -33,8 +35,24 @@ namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.RuntimeFailure
 		private static void Analyze(SyntaxNodeAnalysisContext context)
 		{
 			var finalizer = (DestructorDeclarationSyntax)context.Node;
+			var body = finalizer.Body;
+			var children = body != null ? body.ChildNodes() : Array.Empty<SyntaxNode>();
+			if (children.Any() && children.All(IsDisposeCall))
+			{
+				return;
+			}
 			var loc = finalizer.GetLocation();
 			context.ReportDiagnostic(Diagnostic.Create(Rule, loc));
+		}
+
+		private static bool IsDisposeCall(SyntaxNode node)
+		{
+			if (node is ExpressionStatementSyntax { Expression: InvocationExpressionSyntax invocation })
+			{
+				return invocation.Expression.ToString() == "Dispose";
+			}
+
+			return false;
 		}
 	}
 }
