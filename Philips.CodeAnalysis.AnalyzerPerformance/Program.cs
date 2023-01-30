ac36268a0c130f71ed1ee70291d3bf1a01dba6f3
Collamore, Brian <brian.collamore@philips.com>
2023-01-30 13:59:57 -0500

    analyzer performance metrics

---
+++ Philips.CodeAnalysis.AnalyzerPerformance/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Build.Logging.StructuredLogger;
+
+public class BinaryLogReadBuild
+{
+	public static void Main(string[] args)
+	{
+		if (args.Length == 0)
+		{
+			Console.Error.WriteLine(@"Please specify a .binlog file.");
+		}
+
+		Build buildRoot = BinaryLog.ReadBuild(args[0]);
+		BuildAnalyzer.AnalyzeBuild(buildRoot);
+
+		foreach(BaseNode node in buildRoot.Children)
+		{
+			if (node is NamedNode namedNode && namedNode.Name == @"Analyzer Summary")
+			{
+				foreach (BaseNode analyzerPackageNode in namedNode.Children)
+				{
+					if (analyzerPackageNode is Folder namedAnalyzerPackageFolder && namedAnalyzerPackageFolder.Name.Contains(@"Microsoft.CodeAnalysis"))
+					{
+						foreach (BaseNode analyzerMessage in namedAnalyzerPackageFolder.Children)
+						{
+							if (analyzerMessage is Item item)
+							{
+								string[] analyzerAndId = item.Name.Split(" ");
+								string id = analyzerAndId[1].Substring(1, analyzerAndId[1].Length - 2);
+								Console.WriteLine($"| {id} | {analyzerAndId[0]} | {item.Text} |");
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
