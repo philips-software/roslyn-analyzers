873195d9f97f009103fe9dd9f83dd6023b42bf09
Ynse Hoornenborg <ynse.hoornenborg@philips.com>
2023-02-01 21:11:56 +0100

    Ignore generated code

--- Philips.CodeAnalysis.MaintainabilityAnalyzers/RuntimeFailure/UnmanagedObjectsNeedDisposingAnalyzer.cs
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/RuntimeFailure/UnmanagedObjectsNeedDisposingAnalyzer.cs
@@ -44,7 +44,7 @@ namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.RuntimeFailure
 		/// </summary>
 		public override void Initialize(AnalysisContext context)
 		{
-			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
 			context.EnableConcurrentExecution();
 			context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.FieldDeclaration);
 		}
