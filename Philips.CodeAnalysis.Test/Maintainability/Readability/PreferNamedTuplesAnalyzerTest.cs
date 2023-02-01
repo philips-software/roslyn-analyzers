91a5b6c340ad683d13655d124bdc7c99e5a47bee
Brian Collamore <57269455+bcollamore@users.noreply.github.com>
2023-02-01 05:45:42 -0500

    test: dogfood PH2103 (#332)
    Co-authored-by: Ynse Hoornenborg <ynse.hoornenborg@philips.com>

--- Philips.CodeAnalysis.Test/Maintainability/Readability/PreferNamedTuplesAnalyzerTest.cs
+++ Philips.CodeAnalysis.Test/Maintainability/Readability/PreferNamedTuplesAnalyzerTest.cs
@@ -58,7 +58,8 @@ class Foo
 		public void ErrorIfTupleElementsDoNotHaveNames(string argument)
 		{
 			var source = CreateFunction(argument);
-			VerifyDiagnostic(source, DiagnosticResultHelper.CreateArray(DiagnosticIds.PreferTuplesWithNamedFields).Append(DiagnosticIds.PreferTuplesWithNamedFields));
+			var expected = DiagnosticResultHelper.CreateArray(DiagnosticIds.PreferTuplesWithNamedFields).Append(DiagnosticIds.PreferTuplesWithNamedFields);
+			VerifyDiagnostic(source, expected);
 		}
 
 		[DataRow("(int Foo, int)")]
