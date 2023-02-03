333f565ca83698bbfd5239f6dfd88aecdfc8e96e
Ynse Hoornenborg <ynse.hoornenborg@philips.com>
2023-02-03 16:47:48 +0100

    dogfood again

--- Philips.CodeAnalysis.MsTestAnalyzers/TestHasCategoryCodeFixProvider.cs
+++ Philips.CodeAnalysis.MsTestAnalyzers/TestHasCategoryCodeFixProvider.cs
@@ -61,7 +61,8 @@ namespace Philips.CodeAnalysis.MsTestAnalyzers
 		{
 			SyntaxList<AttributeListSyntax> attributeLists = method.AttributeLists;
 
-			foreach (AttributeListSyntax attributes in attributeLists.Where(a => a.Attributes.Any(attributeSyntax => attributeSyntax.Name.ToString().Contains(@"TestCategory"))))
+			if (attributeLists.Any(list => 
+				    list.Attributes.Any(attributeSyntax => attributeSyntax.Name.ToString().Contains(@"TestCategory"))))
 			{
 				return document;
 			}
