34413418faf7ef0aa61f8dd3f537c6b09613cad1
Ynse Hoornenborg <ynse.hoornenborg@philips.com>
2023-02-05 15:38:44 +0100

    Intermediate state

---
+++ Philips.CodeAnalysis.Test/Security/RegexNeedsTimeoutAnalyzerTest.cs
@@ -0,0 +1,67 @@
+﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Philips.CodeAnalysis.Common;
+using Philips.CodeAnalysis.SecurityAnalyzers;
+using Philips.CodeAnalysis.Test.Helpers;
+using Philips.CodeAnalysis.Test.Verifiers;
+
+namespace Philips.CodeAnalysis.Test.Security
+{
+	[TestClass]
+	public class RegexNeedsTimeoutAnalyzerTest : DiagnosticVerifier
+	{
+		protected override DiagnosticAnalyzer GetDiagnosticAnalyzer()
+		{
+			return new RegexNeedsTimeoutAnalyzer();
+		}
+
+		private string GetTemplate()
+		{
+			return @"
+using System.Text.RegularExpressions;
+namespace RegexNeedsTimeoutTest
+{{
+  public class Foo 
+  {{
+    public Regex MethodA()
+    {{
+      Regex regex = new {0};
+      return regex;
+    }}
+  }}
+}}
+";
+		}
+
+		[DataTestMethod]
+		[DataRow(@"("".*"", RegexOptions.Compiled | RegexOptions.SingleLine)")]
+		[DataRow(@"("".*"")")]
+		[DataRow(@"Regex("".*"", RegexOptions.Compiled | RegexOptions.SingleLine)")]
+		[DataRow(@"Regex("".*"")")]
+		[DataRow(@"System.Text.RegularExpressions.Regex("".*"", RegexOptions.Compiled | RegexOptions.SingleLine)")]
+		[DataRow(@"System.Text.RegularExpressions.Regex("".*"")")]
+		[TestCategory(TestDefinitions.UnitTests)]
+		public void WithoutTimeoutShouldTriggerDiagnostic(string content)
+		{
+			var format = GetTemplate();
+			string testCode = string.Format(format, content);
+			var expected = DiagnosticResultHelper.Create(DiagnosticId.RegexNeedsTimeout);
+			VerifyDiagnostic(testCode, expected);
+		}
+
+		[DataTestMethod]
+		[DataRow(@"Regex("".*"", RegexOptions.Compiled, TimeSpan.FromSeconds(1))")]
+		[DataRow(@"Regex("".*"", RegexOptions.NonBacktracking)")]
+		[DataRow(@"System.Text.RegularExpressions.Regex("".*"", RegexOptions.Compiled, TimeSpan.FromSeconds(1))")]
+		[DataRow(@"System.Text.RegularExpressions.Regex("".*"", RegexOptions.NonBacktracking)")]
+		[TestCategory(TestDefinitions.UnitTests)]
+		public void WithTimeoutShouldNotTriggerDiagnostic(string content)
+		{
+			var format = GetTemplate();
+			string testCode = string.Format(format, content);
+			VerifySuccessfulCompilation(testCode);
+		}
+	}
+}
