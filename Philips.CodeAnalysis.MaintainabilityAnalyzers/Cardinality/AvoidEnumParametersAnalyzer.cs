31bac656cd7a8a62e650fe3a37ef73551bcba8ab
Rodolfo Hansen <rhansen@kitsd.com>
2023-02-07 19:51:17 +0100

    feat: avoid enums in parameters
    The second of the Cardinality inspired maintainability analyzers.

---
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/Cardinality/AvoidEnumParametersAnalyzer.cs
@@ -0,0 +1,45 @@
+﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Philips.CodeAnalysis.Common;
+using static LanguageExt.Prelude;
+
+namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.Cardinality
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class AvoidEnumParametersAnalyzer : SingleDiagnosticAnalyzer<MethodDeclarationSyntax, AvoidEnumParametersSyntaxNodeAction>
+	{
+
+		private static readonly string Title = @"Methods should not accept enums";
+		private static readonly string MessageFormat = @"Method {1} should be refactored away. Illegal Enum Parameter {0}";
+		private static readonly string Description = @"Methods taking an enum as a parameter should instead live inside said enum.";
+
+		public AvoidEnumParametersAnalyzer()
+			: base(DiagnosticId.AvoidEnumParameters, Title, MessageFormat, Description, Categories.FunctionalProgramming, isEnabled: false)
+		{ }
+	}
+
+	public class AvoidEnumParametersSyntaxNodeAction : SyntaxNodeAction<MethodDeclarationSyntax>
+	{
+		public override void Analyze()
+		{
+			_ = List(Node)
+				.Filter((m) => !m.IsOverridden())
+				.SelectMany(AnalyzeMethodParameters)
+				.Iter(Context.ReportDiagnostic);
+		}
+
+		private IEnumerable<Diagnostic> AnalyzeMethodParameters(MethodDeclarationSyntax m)
+		{
+			return m.ParameterList.Parameters
+				.Filter((p) => p.IsEnum(Context))
+				.Select((p) => Diagnostic.Create(Rule, p.GetLocation(), p.Identifier.Text, m.Identifier.Text));
+		}
+	}
+
+}
---
+++ Philips.CodeAnalysis.Test/Cardinality/AvoidEnumParametersTest.cs
@@ -0,0 +1,139 @@
+﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Philips.CodeAnalysis.MaintainabilityAnalyzers.Cardinality;
+using Philips.CodeAnalysis.Test.Helpers;
+using Philips.CodeAnalysis.Test.Verifiers;
+
+namespace Philips.CodeAnalysis.Test.Cardinality
+{
+	[TestClass]
+	public class AvoidEnumParametersTest : DiagnosticVerifier
+	{
+		//No diagnostics expected to show up
+		[TestMethod]
+		[TestCategory(TestDefinitions.UnitTests)]
+		public async Task NotFireForEmptyFiles()
+		{
+			var test = "";
+
+			await VerifySuccessfulCompilation(test);
+		}
+
+		//No diagnostics expected to show up
+		[TestMethod]
+		[TestCategory(TestDefinitions.UnitTests)]
+		public async Task NotFireForNonVoidMethods()
+		{
+			var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class MyClass
+        {   
+            public bool Foo(bool b) { return true; }
+        }
+    }";
+
+			await VerifySuccessfulCompilation(test);
+		}
+
+		[TestMethod]
+		[TestCategory(TestDefinitions.UnitTests)]
+		public async Task FireForEnumParam()
+		{
+			var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };  
+
+        class MyClass
+        {   
+            public void Foo(Day d) {}
+        }
+    }";
+
+			await VerifyDiagnostic(test, regex: "Method Foo.*Parameter d");
+		}
+
+		[TestMethod]
+		[TestCategory(TestDefinitions.UnitTests)]
+		public async Task NotFireForInterfaceParam()
+		{
+			var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+
+
+        class MyClass
+        {   
+            public void Foo(IEnumerable<string> d) {}
+        }
+    }";
+
+			await VerifySuccessfulCompilation(test);
+		}
+
+		/**
+         * The reasoning behind this condition is that its impossible to avoid void methods
+         * when they override those of base class
+         */
+		[TestMethod]
+		[TestCategory(TestDefinitions.UnitTests)]
+		public async Task NotFireForProductParam()
+		{
+			var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        public class ProductOf
+	{
+		public bool Red;
+        public int Blue;
+	}
+
+    public class ProductUse
+    {
+        public bool Foo(ProductOf param)
+        {
+            return true;
+        }
+    }
+    }";
+			await VerifySuccessfulCompilation(test);
+		}
+
+		protected override DiagnosticAnalyzer GetDiagnosticAnalyzer()
+		{
+			return new AvoidEnumParametersAnalyzer();
+		}
+	}
+}
