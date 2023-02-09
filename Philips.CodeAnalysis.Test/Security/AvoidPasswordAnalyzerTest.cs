5c45f74be377c3315cbb934cf55c7c6a37da7864
Collamore, Brian <brian.collamore@philips.com>
2023-02-09 11:16:07 -0500

    test: fix dogfood findings

--- Philips.CodeAnalysis.Test/Security/AvoidPasswordAnalyzerTest.cs
+++ Philips.CodeAnalysis.Test/Security/AvoidPasswordAnalyzerTest.cs
@@ -1,6 +1,9 @@
 ﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
 
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Philips.CodeAnalysis.Common;
