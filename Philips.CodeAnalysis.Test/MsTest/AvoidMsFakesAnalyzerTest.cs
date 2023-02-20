2a84a43db9d73456fa0c841149a27fc85a705d21
Brian Collamore <57269455+bcollamore@users.noreply.github.com>
2023-02-20 12:44:25 -0500

    Update AvoidMsFakesAnalyzerTest.cs

--- Philips.CodeAnalysis.Test/MsTest/AvoidMsFakesAnalyzerTest.cs
+++ Philips.CodeAnalysis.Test/MsTest/AvoidMsFakesAnalyzerTest.cs
@@ -9,9 +9,6 @@ using Philips.CodeAnalysis.Test.Verifiers;
 
 namespace Philips.CodeAnalysis.Test.MsTest
 {
-	/// <summary>
-	/// 
-	/// </summary>
 	[TestClass]
 	public class AvoidMsFakesAnalyzerTest : DiagnosticVerifier
 	{
