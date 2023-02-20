a9f5c1f117f2746d874ba4d039c06f5126066f79
Collamore, Brian <brian.collamore@philips.com>
2023-02-20 15:25:03 -0500

    fix build

--- Philips.CodeAnalysis.Test/Common/AllowedTypeSymbolsTest.cs
+++ Philips.CodeAnalysis.Test/Common/AllowedTypeSymbolsTest.cs
@@ -1,7 +1,9 @@
 ﻿// © 2022 Koninklijke Philips N.V. See License.md in the project root for license information.
 
 using System.Collections.Immutable;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
