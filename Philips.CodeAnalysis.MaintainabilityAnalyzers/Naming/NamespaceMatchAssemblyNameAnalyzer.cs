b070157ea64e83eab3c255ac748ac3e96f365cdf
Ynse Hoornenborg <ynse.hoornenborg@philips.com>
2023-02-20 18:21:29 +0100

    docs: Higher number docs (#454)

--- Philips.CodeAnalysis.MaintainabilityAnalyzers/Naming/NamespaceMatchAssemblyNameAnalyzer.cs
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/Naming/NamespaceMatchAssemblyNameAnalyzer.cs
@@ -1,11 +1,7 @@
 ﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
 
 using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Philips.CodeAnalysis.Common;
