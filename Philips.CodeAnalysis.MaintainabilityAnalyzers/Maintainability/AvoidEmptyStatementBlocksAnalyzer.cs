5d9399716b0dc653f5e49d020844feb380eddb89
Brian Collamore <57269455+bcollamore@users.noreply.github.com>
2023-02-26 16:54:01 -0500

    fix: experiment

--- Philips.CodeAnalysis.MaintainabilityAnalyzers/Maintainability/AvoidEmptyStatementBlocksAnalyzer.cs
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/Maintainability/AvoidEmptyStatementBlocksAnalyzer.cs
@@ -7,7 +7,6 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Philips.CodeAnalysis.Common;
 
-
 namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.Maintainability
 {
 	[DiagnosticAnalyzer(LanguageNames.CSharp)]
