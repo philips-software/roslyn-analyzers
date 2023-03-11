0e3a6a62fe120e8e061e60c4d448e6ade1950902
Ynse Hoornenborg <ynse.hoornenborg@philips.com>
2023-03-11 10:27:44 +0100

    test: Improve test coverage for AdditionalFilesHelper

---
+++ Philips.CodeAnalysis.Test/Helpers/TestAdditionalFile.cs
@@ -0,0 +1,26 @@
+﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Philips.CodeAnalysis.Test.Helpers
+{
+	public class TestAdditionalFile : AdditionalText
+	{
+		private readonly SourceText _text;
+
+		public TestAdditionalFile(string path, SourceText text)
+		{
+			Path = path;
+			_text = text;
+		}
+
+		public override SourceText GetText(CancellationToken cancellationToken = new CancellationToken())
+		{
+			return _text;
+		}
+
+		public override string Path { get; }
+	}
+}
