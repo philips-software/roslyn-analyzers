50deea9f641f0c02fb07735c51eb0d200efdab15
Ynse Hoornenborg <ynse.hoornenborg@philips.com>
2023-11-07 17:14:02 +0100

    feat: avoid excludefromcodecoverage (#702)

---
+++ Philips.CodeAnalysis.MaintainabilityAnalyzers/Maintainability/AvoidExcludeFromCodeCoverageAnalyzer.cs
@@ -0,0 +1,42 @@
+﻿// © 2023 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Philips.CodeAnalysis.Common;
+
+namespace Philips.CodeAnalysis.MaintainabilityAnalyzers.Maintainability
+{
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class AvoidExcludeFromCodeCoverageAnalyzer : SingleDiagnosticAnalyzer<AttributeListSyntax, AvoidExcludeFromCodeCoverageAnalyzerSyntaxNodeAction>
+	{
+		private const string Title = @"Avoid the ExcludeFromCodeCoverage attribute";
+		public const string MessageFormat = Title;
+		private const string Description = MessageFormat;
+
+		public AvoidExcludeFromCodeCoverageAnalyzer()
+			: base(DiagnosticId.AvoidExcludeFromCodeCoverage, Title, MessageFormat, Description, Categories.Maintainability)
+		{ }
+	}
+
+	public class AvoidExcludeFromCodeCoverageAnalyzerSyntaxNodeAction : SyntaxNodeAction<AttributeListSyntax>
+	{
+		private const string ExcludeFromCodeCoverageAttributeTypeName = "ExcludeFromCodeCoverage";
+		public override void Analyze()
+		{
+			IReadOnlyDictionary<string, string> aliases = Helper.ForNamespaces.GetUsingAliases(Node);
+			foreach (AttributeSyntax attribute in Node.Attributes.Where(attr => IsExcludeFromCodeCoverage(aliases, attr)))
+			{
+				Location location = Node.GetLocation();
+				ReportDiagnostic(location);
+			}
+		}
+
+		private bool IsExcludeFromCodeCoverage(IReadOnlyDictionary<string, string> aliases, AttributeSyntax attribute)
+		{
+			return attribute.Name.GetFullName(aliases).Contains(ExcludeFromCodeCoverageAttributeTypeName);
+		}
+	}
+}
