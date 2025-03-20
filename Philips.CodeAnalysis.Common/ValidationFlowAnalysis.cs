c0087a1f4da7ca35d409f7fdbe9b89a7614bb95d
Ynse Hoornenborg <ynse.hoornenborg@philips.com>
2025-03-20 15:09:54 +0100

    Initial validation flow analysis

---
+++ Philips.CodeAnalysis.Common/ValidationFlowAnalysis.cs
@@ -0,0 +1,60 @@
+﻿
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Philips.CodeAnalysis.Common
+{
+	public class ValidationFlowAnalysis
+	{
+		private readonly HashSet<string> _connectedToReturn;
+
+		public ValidationFlowAnalysis(BaseMethodDeclarationSyntax method)
+		{
+			_connectedToReturn = [];
+			IEnumerable<SyntaxToken> parameters = method.ParameterList.Parameters.Select(para => para.Identifier);
+			List<IdentifierNameSyntax> returnVariable = FindReturnVariable(method);
+			FindConnected(parameters, returnVariable);
+		}
+
+		/// <summary>
+		/// Gets a list of all input parameters that are directly influencing one of the return variables.
+		/// </summary>
+		public ImmutableList<string> ConnectedToReturn => _connectedToReturn.ToImmutableList();
+
+		private void FindConnected(IEnumerable<SyntaxToken> tokens, List<IdentifierNameSyntax> returnedVariables)
+		{
+			var returnedNames = new HashSet<string>();
+			foreach (IdentifierNameSyntax ret in returnedVariables)
+			{
+				_ = returnedNames.Add(ret.Identifier.Text);
+			}
+			foreach (SyntaxToken token in tokens)
+			{
+				if (returnedNames.Contains(token.Text))
+				{
+					_ = _connectedToReturn.Add(token.Text);
+				}
+			}
+		}
+
+		private List<IdentifierNameSyntax> FindReturnVariable(BaseMethodDeclarationSyntax method)
+		{
+			IEnumerable<SyntaxNode> statements = method.Body?.DescendantNodes().Where(node => node.IsKind(SyntaxKind.ReturnStatement));
+			return statements?.Select(statement => FindReferencedVariable(((ReturnStatementSyntax)statement).Expression)).ToList();
+		}
+
+		private IdentifierNameSyntax FindReferencedVariable(ExpressionSyntax expression)
+		{
+			if (expression is IdentifierNameSyntax identifierName)
+			{
+				return identifierName;
+			}
+
+			return null;
+		}
+	}
+}
---
+++ Philips.CodeAnalysis.Test/Common/ValidationFlowAnalysisTest.cs
@@ -0,0 +1,48 @@
+﻿// © 2025 Koninklijke Philips N.V. See License.md in the project root for license information.
+
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Philips.CodeAnalysis.Common;
+using Philips.CodeAnalysis.Test.Helpers;
+
+namespace Philips.CodeAnalysis.Test.Common
+{
+	/// <summary>
+	/// Test class for methods in <see cref="ValidationFlowAnalysis"/>.
+	/// </summary>
+	[TestClass]
+	public class ValidationFlowAnalysisTest
+	{
+		private const string SimpleConnectedMethod = @"
+public int ConnectedMethod(int a) {
+	return a;
+}
+";
+
+
+		[DataTestMethod]
+		[DataRow(SimpleConnectedMethod, DisplayName = nameof(SimpleConnectedMethod))]
+		[TestCategory(TestDefinitions.UnitTests)]
+		public void ExpectParameterAToBeConnected(string code)
+		{
+			// Arrange
+			CSharpCompilation compilation = CreateCompilation(code);
+			var method = compilation.SyntaxTrees[0].GetRoot().DescendantNodes()
+				.First(node => node.IsKind(SyntaxKind.MethodDeclaration)) as BaseMethodDeclarationSyntax;
+			// Act
+			var analysis = new ValidationFlowAnalysis(method);
+			// Assert
+			CollectionAssert.Contains(analysis.ConnectedToReturn, "a");
+		}
+
+		private static CSharpCompilation CreateCompilation(string code)
+		{
+			SyntaxTree tree = SyntaxFactory.ParseSyntaxTree(code);
+			return CSharpCompilation.Create("Test.dll", new[] { tree });
+		}
+	}
+}
